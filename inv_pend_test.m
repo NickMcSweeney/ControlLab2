cc8c5025c493344e2d426997b4d7fa46905ba9a7
Arthur Stolz <arthur.stolz@outlook.com>
2019-11-26 14:30:39 +0100

    added pid call

--- inv_pend_test.m
+++ inv_pend_test.m
@@ -10,9 +10,13 @@ inverted_pendulum.dt_= 5*1e-3;   %set the sampling rate
 tf=8;
 t=linspace(0,tf,tf/inverted_pendulum.dt_);
 
+G = [0; 0; 0];
+
+
 for i=1:length(t)
 	tic;
-	inverted_pendulum.u_ = 0; %set the control input at the current time step
+    e = 0 - inverted_pendulum.x_(1)
+	inverted_pendulum.u_ = inverted_pendulum.my_pid(inverted_pendulum, e, G); %set the control input at the current time step
 		
 	inverted_pendulum.x_=inverted_pendulum.step; %integrate forward according to x_new=f(x,u,dt) and update the state vector
 	duration=toc;
