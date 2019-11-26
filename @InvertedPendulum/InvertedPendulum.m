63ea19da1d7605303d3859cfef5848a72ad5b116
Nicholas Shindler <shindler25@gmail.com>
2019-11-26 14:16:13 +0100

    added pid

--- @InvertedPendulum/InvertedPendulum.m
+++ @InvertedPendulum/InvertedPendulum.m
@@ -1,10 +1,13 @@
 classdef InvertedPendulum
-	
+
 	properties (Access = protected)
 		ax_
 		trans_
+
+        old_e
+        E
 	end
-	
+
 	properties  (Access = public)
 	    g_  %gravity
 		l_  %pendulum length
@@ -13,28 +16,37 @@ classdef InvertedPendulum
 		u_  %control
 		dt_ %sampling rate [s]
 	end
-	
+
 	methods (Access = protected)
 		function dx=dynamics(inverted_pendulum, t,x)
 			dx=[x(2); inverted_pendulum.g_/inverted_pendulum.l_*sin(x(1))+inverted_pendulum.c_*inverted_pendulum.u_];
 		end
-		
+
 		inverted_pendulum = init(inverted_pendulum);
 	end
-	
+
 	methods
-		
+
+        function u = my_pid(obj, e, G)
+            [kp;ki;kd] = G;
+            de = (e - obj.old_e)/obj.dt_;
+            obj.E = e*obj.dt + obj.E;
+
+            u = kp * e + ki * E + kd * de;
+            obj.old_e = e;
+        end
+
 		function inverted_pendulum = InvertedPendulum(ax)
 			if nargin == 0
 				inverted_pendulum.ax_ = axes;
 			else
 				inverted_pendulum.ax_=ax;
 			end
-			
-			
+
+
 			inverted_pendulum = inverted_pendulum.init;
 		end
-		
+
 		function inverted_pendulum = set.x_(inverted_pendulum, x)
 			assert(isvector(x));
 			assert(numel(x) == 2);
@@ -43,9 +55,9 @@ classdef InvertedPendulum
 			set(inverted_pendulum.trans_,'Matrix',Rz);
 			drawnow;
 		end
-		
+
 		x_new=step(inverted_pendulum);
-		
+
 	end
-	
+
 end
