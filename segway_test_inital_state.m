3a056b265164477695bbcd82ed10518b6f8a4f10
Nicholas Shindler <shindler25@gmail.com>
2019-11-30 17:10:41 +0100

    find scalar for maximal angle

--- segway_test_inital_state.m
+++ segway_test_inital_state.m
@@ -3,11 +3,11 @@ clear all; close all; clc;
 segway=Segway; %create a 2D segway instance
 axis([-3,3,-1.5,1.5]); pbaspect([3 1.5 1]); grid on; %adjust the visualization settings
 
-segway.x_=[0.01; 0; 0.3; 0]*2.5; %set the initial state (x=[x; dx; theta; dtheta])
+segway.x_=[0.01; 0; 0.3; 0]*3.4; %set the initial state (x=[x; dx; theta; dtheta])
 segway.dt_= 2*1e-3;   %set the sampling rate
 
 %Simulation duration
-tf=8;
+tf=6;
 t=linspace(0,tf,tf/segway.dt_);
 
 k1 =   -2.2599;
@@ -19,8 +19,6 @@ K = [k1 k2 k3 k4];
 
 a_vec = zeros(1,length(t));
 a_vec = zeros(2,length(t));
-
-segway.x_=[0.01; 0; 0.3; 0]*2; %set the initial state (x=[x; dx; theta; dtheta])
 
 for i=1:length(t)
 	segway.u_ = -K*segway.x_; %set the control input at the current time step
@@ -38,9 +36,9 @@ clf;
 segway=Segway; %create a 2D segway instance
 axis([-3,3,-1.5,1.5]); pbaspect([3 1.5 1]); grid on; %adjust the visualization settings
 
-segway.x_=[0.01; 0; 0.3; 0]*1.5; %set the initial state (x=[x; dx; theta; dtheta])
+segway.x_=[0.01; 0; 0.3; 0]*3.35; %set the initial state (x=[x; dx; theta; dtheta])
 segway.dt_= 2*1e-3;   %set the sampling rate
-tf=3;
+tf=6;
 t=linspace(0,tf,tf/segway.dt_);
 
 b_vec = zeros(1,length(t));
@@ -60,9 +58,9 @@ clf;
 segway=Segway; %create a 2D segway instance
 axis([-3,3,-1.5,1.5]); pbaspect([3 1.5 1]); grid on; %adjust the visualization settings
 
-segway.x_=[0.01; 0; 0.3; 0]; %set the initial state (x=[x; dx; theta; dtheta])
+segway.x_=[0.01; 0; 0.3; 0]*3.3; %set the initial state (x=[x; dx; theta; dtheta])
 segway.dt_= 2*1e-3;   %set the sampling rate
-tf=3;
+tf=6;
 t=linspace(0,tf,tf/segway.dt_);
 
 c_vec = zeros(1,length(t));
@@ -78,14 +76,14 @@ for i=1:length(t)
 	pause(segway.dt_-t); %a crude way of making the visualization appear in real-time
 end
 close all; clf;
-plot(1:length(b_vec(1,:)*2*1e-3),b_vec(1,:), 'r-.');
+plot(1:length(a_vec(1,:)*2*1e-3),a_vec(1,:), 'g--');
 hold on;
-plot(1:length(a_vec(1,:)*2*1e-3),c_vec(1,:), 'g--');
+plot(1:length(b_vec(1,:)*2*1e-3),b_vec(1,:), 'r-.');
 plot(1:length(c_vec(1,:)*2*1e-3),c_vec(1,:), 'k--.');
 hold off;
 pause; clf;
-plot(1:length(b_vec(2,:)*2*1e-3),b_vec(2,:), 'r-.');
+plot(1:length(a_vec(2,:)*2*1e-3),a_vec(2,:), 'g--');
 hold on;
-plot(1:length(a_vec(2,:)*2*1e-3),c_vec(2,:), 'g--');
+plot(1:length(b_vec(2,:)*2*1e-3),b_vec(2,:), 'r-.');
 plot(1:length(c_vec(2,:)*2*1e-3),c_vec(2,:), 'k--.');
 hold off;
